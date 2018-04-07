c03920849628847a16e09483b72325054099b366
vunguyen <you@example.com>
2018-04-07 14:41:41 +0700

    first  commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// server/server.js
+var dotenv = require('dotenv')
+var express  = require('express')
+var router = require('./routes/routes.js')
+var path = require('path')
+var bodyParser = require('body-parser')
+var app = express()
+var mongoose = require('mongoose')
+app.set('view engine' , 'ejs')
+app.set('views', path.join(__dirname, '../client'))
+app.use(express.static(path.join(__dirname, '../client')))
+// use mongoose
+app.use(bodyParser.json({limit:'50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended : false}))
+// mongodb://<dbuser>:<dbpassword>@ds237379.mlab.com:37379/db101
+dotenv.config()
+// console.log('AAA',process.env.MONGOLAB_URI)
+mongoose.connect(process.env.MONGOLAB_URI)
+app.use('/', router)
+module.exports = app
