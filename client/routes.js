19bf595ba13960f8e9d1a7360c389da23cb77ae7
vunguyen <you@example.com>
2018-04-07 16:08:12 +0700

    add

--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import App from './components/App';
+import AppDemo from './components/AppDemo';
 export const Routes = () => (
     <Switch>
-        <Route exact path='/' component={App} />
+        <Route exact path='/' component={AppDemo} />
     </Switch>
 );
 export default Routes;
